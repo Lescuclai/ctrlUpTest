0f7da94528111253fcf858299ec966a5c67fc90b
Lescudie <lescudie@airintservices.fr>
2022-04-18 11:12:00 +0200

    simplify store

--- src/config/store.js
+++ src/config/store.js
@@ -1,12 +1,5 @@
 import { compose, createStore } from "redux";
 
-import { persistStore } from "redux-persist";
+import appReducer from "./reducer";
 
-import rootReducer from "config/reducers";
-
-// Redux chrome debugger setup
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(rootReducer, composeEnhancers);
-
-export const persistor = persistStore(store);
+export const store = createStore(appReducer || {}, compose);
