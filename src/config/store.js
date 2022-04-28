868c6d5e59f1bc17ad7ff7a52b8fadb4fcf31f27
Lescudie <lescudie@airintservices.fr>
2022-04-28 14:38:45 +0200

    try to fix connection by removing persistStore and persistReducer

--- src/config/store.js
+++ src/config/store.js
@@ -1,17 +1,7 @@
 import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import { persistStore } from "redux-persist";
 
 import appReducer from "./reducer";
 
-const persistConfig = {
-  key: "root",
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, appReducer);
-
-export const store = createStore(persistedReducer, {}, composeWithDevTools());
-
-export const persistor = persistStore(store);
+export const store = createStore(appReducer, {}, composeWithDevTools());
